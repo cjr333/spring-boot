d407ae3e5012bc08e95a3e0d8088934ebffd4b0c
cjr333 <25564688+cjr333@users.noreply.github.com>
2020-09-11 13:53:21 +0900

    Context 테스트 코드 추가

--- src/main/java/sample/controller/SampleController.java
+++ src/main/java/sample/controller/SampleController.java
@@ -1,16 +1,27 @@
 package sample.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import sample.service.SampleService;
 
 import java.util.Collections;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class SampleController {
+  private final SampleService sampleService;
+
   @GetMapping("/sample")
   public Mono<List<Integer>> sample() {
     return Mono.just(Collections.singletonList(1));
+  }
+
+  @GetMapping("/log")
+  public Mono<String> log() {
+    return sampleService.log().subscriberContext(ctx -> ctx.put("request", "{request json}"));
   }
 }
